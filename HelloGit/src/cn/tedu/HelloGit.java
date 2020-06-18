88de5c7878677af81dc8f57fbd358d1907589dfd
zhushangling <51771310+zhushangling@users.noreply.github.com>
2020-06-18 09:32:45 +0800

    ddd

--- HelloGit/src/cn/tedu/HelloGit.java
+++ HelloGit/src/cn/tedu/HelloGit.java
@@ -3,9 +3,6 @@ package cn.tedu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("创建git版本控制本地仓库");
-        System.out.println("更新1");
-        System.out.println("更新2");
         System.out.println("在线更新");
-        System.out.println("在线更新2");
     }
 }
